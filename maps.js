ad977d8f9a1c1f79f493124b89b3f3b264c06bfe
Sam Larsen <samlarsen18@gmail.com>
2019-02-14 16:52:00 -0700

    map working

---
+++ maps.js
@@ -0,0 +1,27 @@
+function initMap() {
+  // The location of Uluru
+
+  var latitude = parseFloat(document.getElementById('lat').innerHTML);
+  var longitude = parseFloat(document.getElementById('lng').innerHTML);
+
+  var location = {
+    lat: latitude,
+    lng: longitude
+  };
+
+  // The map, centered at location
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 13,
+      center: location,
+      mapTypeId: 'terrain',
+      disableDefaultUI: true,
+      mapTypeControl: true
+    });
+  console.log(map);
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
